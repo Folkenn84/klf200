91de835bc76b76eb8455853115cf0169b38a5da9
Jean-François Chalony <jean-francois.chalony@liksi.fr>
2020-04-15 21:56:24 +0200

    maj mqtt

--- Mqtt.py
+++ Mqtt.py
@@ -28,45 +28,22 @@ class ThreadReception(threading.Thread):
             if GW_GET_NODE_INFORMATION_NTF == command:
                 print("command traité : ", toHex(slip_unpack(result)))
                 nodeid = int(parse_command(result, 4, 5))
-                position = 100 - int(parse_command(result, 89, 91) / 512)
-                self.mqtt.publish("klf200/node_" + str(nodeid), payload=position)
+                if nodeid == 0:
+                    self.mqtt.publish("klf200/status/" + str(nodeid), payload=toHex(slip_unpack(result)))
+                else:
+                    position = 100 - int(parse_command(result, 89, 91) / 512)
+                    self.mqtt.publish("klf200/status/" + str(nodeid), payload=position)
             elif GW_NODE_STATE_POSITION_CHANGED_NTF == command:
                 print("command traité : ", toHex(slip_unpack(result)))
                 nodeid = int(parse_command(result, 4, 5))
-                position = 100 - int(parse_command(result, 6, 8) / 512)
-                self.mqtt.publish("klf200/status/" + str(nodeid), payload=position)
+                if nodeid == 0:
+                    self.mqtt.publish("klf200/status/" + str(nodeid), payload=toHex(slip_unpack(result)))
+                else:
+                    position = 100 - int(parse_command(result, 6, 8) / 512)
+                    self.mqtt.publish("klf200/status/" + str(nodeid), payload=position)
             else:
                 print("command non traité : ", toHex(slip_unpack(result)))
 
-
-class ThreadEmission(threading.Thread):
-    """objet thread gérant l'émission des messages"""
-    def __init__(self, conn, mqttComm):
-        threading.Thread.__init__(self)
-        self.connexion = conn	     # réf. du socket de connexion
-        self.mqtt = mqttComm
-
-    def run(self):
-        self.mqtt.loop_start()
-        self.mqtt.subscribe("klf200/action")
-        self.mqtt.connect(MQTT_ADDRESS)
-        self.mqtt.on_message = self.on_message
-
-
-    def on_message(self, client, userdata, message):
-        value = str(message.payload.decode("utf-8")).split(" ")
-        print("traitement", value)
-        if "STATUS" == value[0]:
-            position = positionVolet(self.connexion, int(value[1]))
-            # client.publish("klf200/status/" + value[1], payload=position)
-        elif "MOVE" == value[0]:
-            moveVolet(self.connexion, int(value[2]), int(value[1]))
-        elif "STOP" == value[0]:
-            stopVolet(self.connexion, int(value[1]))
-        # print("message received ", str(message.payload.decode("utf-8")))
-        # print("message topic=", message.topic)
-        # print("message qos=", message.qos)
-        # print("message retain flag=", message.retain)
 
 # ===============================================================================
 #
@@ -78,8 +55,6 @@ def main():
     client.on_message = on_message
     client.loop_start()
     client.subscribe("klf200/action")
-    # th_E = ThreadEmission(connklf200, client)
-    # th_E.start()
     th_R = ThreadReception(connklf200, client)
     th_R.start()
     print("starting reception")
@@ -92,6 +67,7 @@ def main():
     # time.sleep(4)
     # print("pending", connklf200.pending())
     # time.sleep(4)
+    client.publish("klf200/action", "POSITION_VOLET 0")
     client.publish("klf200/action", "POSITION_VOLET 1")
     # client.publish("klf200/action", "MOVE 2 50")
     # client.publish("klf200/action", "STOP 2")
